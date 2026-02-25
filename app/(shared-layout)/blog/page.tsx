ab3e0795ddfa04b091cf1d4d0fea888cf7b036a9
NickC <southstandblue@gmail.com>
2026-02-25 20:45:55 +0000

    yet another small fix

--- app/(shared-layout)/blog/page.tsx
+++ app/(shared-layout)/blog/page.tsx
@@ -90,19 +90,19 @@ async function LoadBlogList() {
   );
 }
 
-// function SkeletonLoadingUi() {
-//   return (
-//     <div className="grid gap-6 md:grid-cols-3 lg:grid-cols-3">
-//       {[...Array(3)].map((_, i) => (
-//         <div className="flex flex-col space-y-3" key={i}>
-//           <Skeleton className="h-48 w-full rounded-xl" />
-//           <div className="space-y-2 flex flex-col">
-//             <Skeleton className="h-6 w-3/4" />
-//             <Skeleton className="h-4 w-full" />
-//             <Skeleton className="h-4 w-2/" />
-//           </div>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
+function SkeletonLoadingUi() {
+  return (
+    <div className="grid gap-6 md:grid-cols-3 lg:grid-cols-3">
+      {[...Array(3)].map((_, i) => (
+        <div className="flex flex-col space-y-3" key={i}>
+          <Skeleton className="h-48 w-full rounded-xl" />
+          <div className="space-y-2 flex flex-col">
+            <Skeleton className="h-6 w-3/4" />
+            <Skeleton className="h-4 w-full" />
+            <Skeleton className="h-4 w-2/" />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
